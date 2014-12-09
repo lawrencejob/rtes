e12375f2ed7bd46e4be13620b112cff5b8d25b7d
unknown <lhrj20@CS61008595.ad.susx.ac.uk>
2014-12-09 00:17:59 +0000

    adds GUI for brightness

--- MAIN.C
+++ MAIN.C
@@ -7,6 +7,11 @@
 
 void homeScreen(void);
 int waitForCharInput(void);
+void drawBrightnessScreen(void);
+void sendNL(void);
+
+#define BRIGHTNESS_INCREMENT 5
+#define BRIGHTNESS_MAX 100
 
 void MAIN_vInit(void)
 {
@@ -52,6 +57,8 @@ void sendString(char *msg);
 void menu_chooseBrightness(void);
 void menu_chooseAnimation(void);
 void resetEmulator(void);
+int waitForDirectionInput(void);
+void setColour(char foreground, char background);
 char transmitBuffer[1023];
 
 void homeScreen(void) {
@@ -60,6 +67,9 @@ void homeScreen(void) {
 	
 	const char *message = "\r\n\tWelcome to Light'o'matic 2000. Prepare for the most gripping PWM\r\n\texperience of your life.Prepare to have your mind blown.\r\n\r\n\tA\tChoose brightness\r\n\tB\tChoose animation\r\n\t\r\n\t";
 	
+	resetEmulator();
+
+	sendNL();
 	sendString(message);
 	choice = waitForCharInput();
 	
@@ -81,17 +91,90 @@ void homeScreen(void) {
 }
 
 void menu_chooseBrightness(void) {
-	unsigned char brightness = 0;
-	sendString("BRIGHTNESS SELECT");
+	unsigned char brightness = 0, loop_break = 0;
+	int direction = 0;
+	/*sendString("BRIGHTNESS SELECT");
 	brightness = (waitForCharInput()-48)*10;
 
 	sendString("\r\n\t\r\n\tYou chose ");
 	//sendString(brightness);
-	GPT1_setBrightness(brightness);
+	GPT1_setBrightness(brightness);*/
+	
+	resetEmulator();
+	//setColour(6, 7);
+
+	sendNL();
+	sendString("BRIGHTNESS SELECT:");
+
+	sendNL();
+	sendString("Use the arrow keys to select the brightness you want.");
+
+	sendNL();
+	sendString("Press enter when you're done.");
+	
+	sendNL();
+	sendNL();
+
+	while(loop_break==0){
+		drawBrightnessScreen();
+		direction = waitForDirectionInput();
+		switch(direction) {
+			case 'C':
+			case 'c':
+				loop_break = 0;
+				if(GPT1_getBrightness()+5 <= BRIGHTNESS_MAX)
+					GPT1_setBrightness(GPT1_getBrightness()+BRIGHTNESS_INCREMENT);
+			break;
+			case 'D':
+			case 'd':
+				loop_break = 0;
+				if(GPT1_getBrightness() >= BRIGHTNESS_INCREMENT)
+					GPT1_setBrightness(GPT1_getBrightness() - BRIGHTNESS_INCREMENT);
+			break;
+			default:
+				loop_break = -1;
+				return;
+			break;
+		}
+	}
+}
+
+void drawBrightnessScreen(void) {
+	unsigned char c;
+	char percentage[3];
+
+	ASC0_vSendData(0xD);
+	sendString("\t");
+
+	sendString("[ ");
+	for (c=0; c<GPT1_getBrightness(); c+=BRIGHTNESS_INCREMENT){
+		sendString("=");
+	}
+	for(c=BRIGHTNESS_MAX; c>GPT1_getBrightness(); c-= BRIGHTNESS_INCREMENT) {
+	 	sendString(" ");
+	}
+	sendString(" ] ");
+
+	sprintf(percentage, "%d", GPT1_getBrightness());
+	sendString(percentage);
+	sendString("% ");
+
+	
+	return;
 }
 
 void menu_chooseAnimation(void) {
 	sendString("ANIMATION SELECT");
+}
+
+void setColour(char foreground, char background){
+	ASC0_vSendData(27);
+	sendString("[3");
+	ASC0_vSendData(foreground);
+	ASC0_vSendData(27);
+	sendString("[4");
+	ASC0_vSendData(background);
+
 }
 
 void resetEmulator(void) {
@@ -102,6 +185,10 @@ void resetEmulator(void) {
 	while(ASC0_cReadyToTransmit() == -1){}
 	ASC0_vSendData(27);
 	sendString("[H");
+
+	/*sendNL();
+	sendString("---------------");
+	sendNL();*/
 }
 
 void sendString(char *msg) {
@@ -112,15 +199,27 @@ void sendString(char *msg) {
 	}
 }
 
+void sendNL(void) {
+	sendString("\r\n\t");
+}
+
 int waitForCharInput(void) {
 	int in;
 	while(ASC0_cReadyToReceive() == -1) {}
-	ASC0_vSendData('x');
+	//ASC0_vSendData('x');
 	in = ASC0_uwGetData();
 	IO_vWritePort(P2, 0xFFFF);
 	
 	return in;
 }
 
-
+int waitForDirectionInput(void) {
+	int in;
+	char loop_break = 0;
+	while(loop_break == 0){
+		in = waitForCharInput();
+		if(in == 'A' || in == 'B' || in == 'C' || in == 'D' || in == 0xD || in == 0xA) return in;
+	}
+	return in;
+}
 
